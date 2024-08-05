0d7c9bdfde449f6c376e4a4ba0e7a2bf15aebd3b
RAHUL A P <rahul.ap@aot-technologies.com>
2024-08-05 03:39:29 -0700

    second

--- client/src/Pages/News.jsx
+++ client/src/Pages/News.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "../App.css"
+import Newscard from '../components/Newscard'
 export default function News() {
+  const apikey = 'cd8fe1b05fb6423cbfc73a6de53f1d99'
   return (
     <div className='news'>
-       <h1>News</h1>
+      <h1>Latest News</h1>
+      <div className='news-area'>
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+        <Newscard />
+      </div>
+
     </div>
   )
 }
---
+++ client/src/components/Newscard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css'
+export default function Newscard() {
+  return (
+    <div className='newscard'>
+        <h2>News Card</h2>
+        <p>This is a news card</p>
+        <button>Read More</button>  
+    </div>
+  )
+}
