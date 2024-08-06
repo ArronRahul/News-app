19760bd747bfa293225ec6784c4c5711cb865745
RAHUL A P <rahul.ap@aot-technologies.com>
2024-08-05 20:42:42 -0700

    news ok

--- client/src/Pages/News.jsx
+++ client/src/Pages/News.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../App.css"
 import Newscard from '../components/Newscard'
 export default function News() {
-  const apikey = 'cd8fe1b05fb6423cbfc73a6de53f1d99'
+  const [NewsData,setNewsData]=useState([])
+
+
+const endpoint = 'https://newsdata.io/api/1/latest?apikey=pub_4988362b664f3cc46f720982769ea356d9a73&q=crypto&sentiment=positive';
+
+const fetchNews = async () => {
+    try {
+        const response = await fetch(`${endpoint}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        setNewsData(data.results);
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+};
+
+useEffect(()=>{
+  fetchNews();
+},[])
+
+  
+
+
   return (
     <div className='news'>
       <h1>Latest News</h1>
       <div className='news-area'>
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
+      {NewsData.map((data, index) => (
+          <Newscard key={data.article_id || index} data={data} /> 
+        ))}
+        
       </div>
 
     </div>
--- client/src/components/Newscard.jsx
+++ client/src/components/Newscard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../App.css'
-export default function Newscard() {
+export default function Newscard({data}) {
   return (
     <div className='newscard'>
-        <h2>News Card</h2>
-        <p>This is a news card</p>
-        <button>Read More</button>  
+        <img src={data.image_url? data.image_url: 'https://www.indiantelevision.com/sites/default/files/styles/smartcrop_800x800/public/images/tv-images/2021/08/02/news.jpg?itok=eEnb05ue'} alt={data.title} />
+        <h2>{data.title}</h2>
+        <p>{data.description}</p> 
     </div>
   )
 }
