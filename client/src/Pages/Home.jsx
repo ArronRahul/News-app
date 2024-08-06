a45eeb97b54d0eb7a2d4f52c8454ddfb2fd5e7b0
RAHUL A P <rahul.ap@aot-technologies.com>
2024-08-05 22:04:48 -0700

    ok

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,8 +3,7 @@ import "../App.css"
 export default function Home() {
   return (
     <div className='home'>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the default page when you navigate to the root URL.</p>
+      <h2>hi , {}</h2>
     </div>
   )
 }
--- client/src/Pages/News.jsx
+++ client/src/Pages/News.jsx
@@ -3,29 +3,24 @@ import "../App.css"
 import Newscard from '../components/Newscard'
 export default function News() {
   const [NewsData,setNewsData]=useState([])
-
-
-const endpoint = 'https://newsdata.io/api/1/latest?apikey=pub_4988362b664f3cc46f720982769ea356d9a73&q=crypto&sentiment=positive';
+const endpoint = 'https://newsdata.io/api/1/latest?apikey=pub_4988362b664f3cc46f720982769ea356d9a73&domainurl=bbc.com';
 
 const fetchNews = async () => {
     try {
         const response = await fetch(`${endpoint}`);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+        if (!response.status) {
+            throw new Error('not ok');
         }
         const data = await response.json();
-        console.log(data);
         setNewsData(data.results);
     } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        console.error('There was a problem :', error);
     }
 };
 
 useEffect(()=>{
   fetchNews();
 },[])
-
-  
 
 
   return (
@@ -35,7 +30,6 @@ useEffect(()=>{
       {NewsData.map((data, index) => (
           <Newscard key={data.article_id || index} data={data} /> 
         ))}
-        
       </div>
 
     </div>
